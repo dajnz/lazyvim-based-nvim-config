dbc007b7faceb901f097413192ccd42d65844398
Vladimir Ponomarev <vladipnm@yandex.ru>
2026-02-06 13:32:29 +0300

    Fix issues with hotkeys for RU keyboard layout

--- lua/plugins/langmapper.lua
+++ lua/plugins/langmapper.lua
@@ -8,6 +8,23 @@ return {
     -- High priority is needed if you will use `autoremap()`
     priority = 1,
 
+    -- A fix for issues when using RU keyboard layout
+    -- @see: https://github.com/Wansmer/langmapper.nvim/discussions/11#discussioncomment-11210683
+    config = function(_, opts)
+        local lm = require "langmapper"
+
+        lm.setup(opts)
+        lm.hack_get_keymap()
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "LazyDone",
+            once = true,
+            callback = function()
+                lm.automapping { global = true, buffer = false }
+            end,
+        })
+    end,
+
     opts = {
         -- Removed letters '~' and '`' because it incorrectly maps them to 'Ё' and 'ё',
         -- but on my keyboard it should be mapped to '|' and '\', and I need '\' for Flash hotkey
--- lua/plugins/which-key---ext.lua
+++ lua/plugins/which-key---ext.lua
@@ -5,6 +5,29 @@
 return {
     {
         'folke/which-key.nvim',
+
+        -- A fix for issues when using RU keyboard layout
+        -- @see: https://github.com/Wansmer/langmapper.nvim/discussions/11#discussioncomment-11210683
+        config = function(_, opts)
+            local cmd = { 'xkblayout-state', 'print', '%s' }
+            local has_cmd = vim.fn.executable(cmd[1]) == 1
+            local lmu = require 'langmapper.utils'
+            local wk_state = require 'which-key.state'
+            local check_orig = wk_state.check
+            ---@diagnostic disable-next-line: duplicate-set-field
+            wk_state.check = function(state, key)
+                if key ~= nil then
+                    local ambiguous = key == '.' or key == ',' or key == '/'
+                    if not ambiguous or has_cmd and vim.system(cmd):wait().stdout == 'ru' then
+                        key = lmu.translate_keycode(key, 'default', 'ru')
+                    end
+                end
+                return check_orig(state, key)
+            end
+
+            require('which-key').setup(opts)
+        end,
+
         opts = {
             spec = {
                 {
