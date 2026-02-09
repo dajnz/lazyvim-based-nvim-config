a6692eef2b150b272c9362999d01a4c956cd2976
Vladimir Ponomarev <vladipnm@yandex.ru>
2026-02-10 01:04:47 +0300

    Add some linters

---
+++ lua/plugins/nvim-lint---ext.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+
+        opts = {
+            linters_by_ft = {
+                fish = { "fish" },
+                make = { "checkmake" },
+                dockerfile = { "hadolint" },
+
+                -- Use the "*" filetype to run linters on all filetypes.
+                -- ['*'] = { 'global linter' },
+
+                -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+                -- ['_'] = { 'fallback linter' },
+                -- ["*"] = { "typos" },
+            },
+        },
+    },
+}
