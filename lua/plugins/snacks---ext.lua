314f43ab04d8b912df3dbc2e4455c9e93bf709b2
Vladimir Ponomarev <vladipnm@yandex.ru>
2026-02-10 01:05:48 +0300

    Exclude vendor and node_modules from snacks file and grep pickers

--- lua/plugins/snacks---ext.lua
+++ lua/plugins/snacks---ext.lua
@@ -3,6 +3,16 @@ return {
         "folke/snacks.nvim",
         opts = {
             scroll = { enabled = false },
-        }
-    }
+            picker = {
+                sources = {
+                    files = {
+                        exclude = { "node_modules", "vendor" },
+                    },
+                    grep = {
+                        exclude = { "node_modules", "vendor" },
+                    },
+                },
+            },
+        },
+    },
 }
